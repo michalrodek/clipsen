19e153e686a8d3f245583f07a6bbe488fc2be7c5
Michal Rodek <michalrodek@Michal--MacBook-Pro.local>
2024-02-23 17:20:42 +0100

    refreshing access token

--- src/lib/fetching.ts
+++ src/lib/fetching.ts
@@ -1,20 +1,33 @@
 import { TwitchApiResponse, StreamData, ClipData } from "@/typings";
+import { kv } from "@vercel/kv";
 
-const options = {
-  headers: {
-    Authorization: `Bearer ${process.env.BEARER}`,
-    "Client-Id": `${process.env.CLIENT_ID}`,
-  },
-  next: {
-    revalidate: 3600,
-  },
-};
+const ACCESS_TOKEN = "access_token";
 
-async function _fetch(url: string) {
+async function getFetchOptions() {
+  return {
+    headers: {
+      Authorization: `Bearer ${await kv.get(ACCESS_TOKEN)}`,
+      "Client-Id": `${process.env.CLIENT_ID}`,
+    },
+    next: {
+      revalidate: 3600,
+    },
+  };
+}
+
+async function _fetch(url: string, options: any) {
   try {
     const resp = await fetch(url, options);
 
     if (!resp.ok) {
+      const error = await resp.json();
+
+      if (error.message === "Invalid OAuth token") {
+        await generateAccessToken();
+
+        return await _fetch(url, await getFetchOptions());
+      }
+
       return undefined;
     }
 
@@ -24,16 +37,35 @@ async function _fetch(url: string) {
   }
 }
 
+async function generateAccessToken() {
+  const resp = await _fetch("https://id.twitch.tv/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: "client_credentials",
+    }),
+  });
+
+  await kv.set(ACCESS_TOKEN, resp.access_token);
+}
+
 export async function getUser(
   name: string
 ): Promise<TwitchApiResponse<StreamData> | undefined> {
-  return _fetch(`https://api.twitch.tv/helix/users/?login=${name}`);
+  return await _fetch(
+    `https://api.twitch.tv/helix/users/?login=${name}`,
+    await getFetchOptions()
+  );
 }
 
 export async function getClips(
   id: string,
   startedAt: Date | undefined,
-  cursor: string,
+  cursor: string
 ): Promise<TwitchApiResponse<ClipData> | undefined> {
   let url = `https://api.twitch.tv/helix/clips?broadcaster_id=${id}`;
 
@@ -44,6 +76,6 @@ export async function getClips(
   if (cursor) {
     url += `&after=${cursor}`;
   }
-  console.log(url);
-  return _fetch(url);
+
+  return await _fetch(url, await getFetchOptions());
 }
